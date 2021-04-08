fc1827d252f9b4fc832d1a9a91f0c0d543a3cae1
Nrect <boss.startov@bk.ru>
2021-04-08 14:05:12 +0200

    init project

---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  .container
+    h1.title.title--primary Starter template
+    logo
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: { Logo }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand',
+  'Source Sans Pro',
+  -apple-system,
+  BlinkMacSystemFont,
+  'Segoe UI',
+  Roboto,
+  'Helvetica Neue',
+  Arial,
+  sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+
+  &--primary {
+    font-weight: bold;
+  }
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
