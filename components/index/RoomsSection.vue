f4372f042e01730a4188226a9ff0e0cd4d09a08e
Nrect <boss.startov@bk.ru>
2021-04-12 17:01:46 +0200

    rooms ~temp

--- components/index/RoomsSection.vue
+++ components/index/RoomsSection.vue
@@ -3,10 +3,34 @@
     .container
       .rooms_section__wrapper
         h2.rooms_section__title Каюты
+        hooper(:wheel-control="false" :settings="hooperSettings").card-slider
+          slide(v-for="x in 10" :key="x").card-slider__slide
+            .room-card
+              img.room-card_img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
+              .room-card__title Двухместная каюта с ванной комнатой
+              .room-card_description Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.
+              button.btn Подробнее
+              .room-card__swipe
+          hooper-navigation(slot="hooper-addons")
 </template>
 
 <script>
 export default {
-  name: 'AppRoomsSection'
+  name: 'AppRoomsSection',
+  data () {
+    return {
+      hooperSettings: {
+        itemsToShow: 3,
+        // centerMode: true,
+        pagination: 'no'
+        // breakpoints: {
+        //   800: {
+        //     centerMode: false,
+        //     itemsToShow: 3
+        //   }
+        // }
+      }
+    }
+  }
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/stylesheets/main.scss'
+    '@/assets/stylesheets/main.scss',
+    'hooper/dist/hooper.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
