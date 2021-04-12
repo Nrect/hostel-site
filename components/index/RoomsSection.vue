adef1934805bd85864d656bd4c287b19026122ab
Aleksandr <boss.startov@bk.ru>
2021-04-12 22:24:12 +0300

    rooms ~temp

--- components/index/RoomsSection.vue
+++ components/index/RoomsSection.vue
@@ -4,9 +4,9 @@
       .rooms_section__wrapper
         h2.rooms_section__title Каюты
         hooper(:wheel-control="false" :settings="hooperSettings").card-slider
-          slide(v-for="x in 10" :key="x").card-slider__slide
+          slide(v-for="x in 5" :key="x").card-slider__slide
             .room-card
-              img.room-card_img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
+              img.room-card__img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
               .room-card__title Двухместная каюта с ванной комнатой
               .room-card_description Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.
               button.btn Подробнее
@@ -21,8 +21,9 @@ export default {
     return {
       hooperSettings: {
         itemsToShow: 3,
-        // centerMode: true,
-        pagination: 'no'
+        pagination: 'no',
+        trimWhiteSpace: true
+        // infiniteScroll: true
         // breakpoints: {
         //   800: {
         //     centerMode: false,
