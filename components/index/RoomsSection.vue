75f8c0ca2a86c0e956604e4383fdd626a90f5bb9
Nrect <boss.startov@bk.ru>
2021-04-13 13:42:19 +0200

    rooms ~temp

--- components/index/RoomsSection.vue
+++ components/index/RoomsSection.vue
@@ -7,7 +7,7 @@
           slide(v-for="x in 5" :key="x").card-slider__slide
             .room-card
               img.room-card__img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
-              .room-card__title Двухместная каюта с ванной комнатой
+              h3.room-card__title Двухместная каюта с ванной комнатой
               .room-card_description Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.
               button.btn Подробнее
               .room-card__swipe
