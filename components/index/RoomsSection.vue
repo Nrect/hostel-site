b9304636e028d8816ec6ad63b47f050b7d61ec88
Nrect <boss.startov@bk.ru>
2021-04-14 16:28:16 +0200

    rooms ~temp

--- components/index/RoomsSection.vue
+++ components/index/RoomsSection.vue
@@ -3,15 +3,23 @@
     .container
       .rooms_section__wrapper
         h2.rooms_section__title Каюты
-        hooper(:wheel-control="false" :settings="hooperSettings").card-slider
-          slide(v-for="x in 5" :key="x").card-slider__slide
-            .room-card
-              img.room-card__img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
-              h3.room-card__title Двухместная каюта с ванной комнатой
-              .room-card_description Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.
-              button.btn Подробнее
-              .room-card__swipe
-          hooper-navigation(slot="hooper-addons")
+        .card-slider
+          hooper(:wheel-control="false" ref="carousel" :settings="hooperSettings")
+            slide(v-for="x in 5" :key="x").card-slider__slide
+              .room-card
+                .room-card__wrapper
+                  .room-card__img
+                    img(src="http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg")
+                  h3.room-card__title Двухместная каюта с ванной комнатой
+                  .room-card__description Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.
+                  span.room-card__btn.btn
+                    nuxt-link(to="/") Подробнее
+        .card-slider__buttons
+          button(@click.prevent="slidePrev").card-slider__btn
+            span <
+          button(@click.prevent="slideNext").card-slider__btn
+            span >
+
 </template>
 
 <script>
@@ -22,15 +30,27 @@ export default {
       hooperSettings: {
         itemsToShow: 3,
         pagination: 'no',
-        trimWhiteSpace: true
-        // infiniteScroll: true
-        // breakpoints: {
-        //   800: {
-        //     centerMode: false,
-        //     itemsToShow: 3
-        //   }
-        // }
+        trimWhiteSpace: true,
+        breakpoints: {
+          1500: {
+            itemsToShow: 3
+          },
+          768: {
+            itemsToShow: 2
+          },
+          320: {
+            itemsToShow: 1
+          }
+        }
       }
+    }
+  },
+  methods: {
+    slidePrev () {
+      this.$refs.carousel.slidePrev()
+    },
+    slideNext () {
+      this.$refs.carousel.slideNext()
     }
   }
 }
