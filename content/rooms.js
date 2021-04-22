c8d6afda82b1eeef0997059518c632ce0bb3923d
Nrect <boss.startov@bk.ru>
2021-04-22 21:15:23 +0200

    photos

--- content/rooms.js
+++ content/rooms.js
@@ -1,13 +1,13 @@
 export const roomsData = [
   {
     slug: 'two-room',
-    imgSrc: 'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg',
+    imgSrc: require('@/assets/images/room2/1.jpg'),
     title: 'Двухместная каюта с ванной комнатой',
     content: 'Отдельный номер для двоих с большой и удобной двуспальной кроватью, шкафом для одежды и телевизором.  В номере имеется просторный собственный сан. узел и душевая.',
     advantages: [
-      'двуспальная кровать',
-      'шкафом для одежды',
-      'телевизор'
+      'Двуспальная кровать',
+      'Шкаф для одежды',
+      'Телевизор'
     ],
     imageSet: [
       require('@/assets/images/room2/1.jpg'),
@@ -24,38 +24,88 @@ export const roomsData = [
   },
   {
     slug: 'six-room',
-    imgSrc: 'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg',
+    imgSrc: require('@/assets/images/room6/2.jpg'),
     title: '6 - местная каюта для женщин с общим сан. узлом',
     content: 'Просторный номер односпальными кроватями и уютными слип-боксами. Для каждого гостя предусмотрены запирающиеся шкафы и место под чемоданы.',
-    advantages: [],
-    imageSet: [],
-    additionContent: []
+    advantages: [
+      'Запирающиеся шкафы',
+      'Место под чемоданы'
+    ],
+    imageSet: [
+      require('@/assets/images/room6/1.jpg'),
+      require('@/assets/images/room6/2.jpg'),
+      require('@/assets/images/room6/3.jpg'),
+      require('@/assets/images/room6/4.jpg'),
+      require('@/assets/images/room6/5.jpg'),
+      require('@/assets/images/room6/6.jpg')
+    ],
+    additionContent: ['Просторный номер односпальными кроватями и уютными слип-боксами.']
   },
   {
     slug: 'eight-room',
-    imgSrc: 'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg',
+    imgSrc: require('@/assets/images/room8/7.jpg'),
     title: '8 - местная каюта для женщин с общим сан. узлом',
     content: 'Просторный номер односпальными кроватями и уютными слип-боксами. Для каждого гостя предусмотрены запирающиеся шкафы и место под чемоданы.',
-    advantages: [],
-    imageSet: [],
-    additionContent: []
+    advantages: [
+      'Запирающиеся шкафы',
+      'Место под чемоданы'
+    ],
+    imageSet: [
+      require('@/assets/images/room8/1.jpg'),
+      require('@/assets/images/room8/2.jpg'),
+      require('@/assets/images/room8/3.jpg'),
+      require('@/assets/images/room8/4.jpg'),
+      require('@/assets/images/room8/5.jpg'),
+      require('@/assets/images/room8/6.jpg'),
+      require('@/assets/images/room8/7.jpg'),
+      require('@/assets/images/room8/8.jpg')
+    ],
+    additionContent: ['Просторный номер односпальными кроватями и уютными слип-боксами.']
   },
   {
     slug: 'ten-room',
-    imgSrc: 'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg',
+    imgSrc: require('@/assets/images/room10/1.jpg'),
     title: '10 - местная каюта для мужчин и женщин с общим сан. узлом',
     content: 'Просторный номер с двуспальными кроватями и уютными слип-боксами. Все кровати закрываются плотной шторкой. Для каждого гостя предусмотрены запирающиеся шкафы и место под чемоданы.',
-    advantages: [],
-    imageSet: [],
-    additionContent: []
+    advantages: [
+      'Запирающиеся шкафы',
+      'Место под чемоданы',
+      'Все кровати закрываются плотной шторкой'
+    ],
+    imageSet: [
+      require('@/assets/images/room10/1.jpg'),
+      require('@/assets/images/room10/2.jpg'),
+      require('@/assets/images/room10/3.jpg'),
+      require('@/assets/images/room10/4.jpg'),
+      require('@/assets/images/room10/5.jpg'),
+      require('@/assets/images/room10/6.jpg'),
+      require('@/assets/images/room10/7.jpg'),
+      require('@/assets/images/room10/8.jpg')
+    ],
+    additionContent: ['Просторный номер с двуспальными кроватями и уютными слип-боксами.']
   },
   {
     slug: 'twelve-room',
-    imgSrc: 'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_01-430x280.jpg',
+    imgSrc: require('@/assets/images/room12/3.jpg'),
     title: '12 - местная каюта для мужчин и женщин с общим сан. узлом',
     content: 'Просторный номер односпальными кроватями и уютными слип-боксами. Для каждого гостя предусмотрены запирающиеся шкафы и место под чемоданы.',
-    advantages: [],
-    imageSet: [],
-    additionContent: []
+    advantages: [
+      'запирающиеся шкафы',
+      'место под чемоданы'
+    ],
+    imageSet: [
+      require('@/assets/images/room12/1.jpg'),
+      require('@/assets/images/room12/2.jpg'),
+      require('@/assets/images/room12/3.jpg'),
+      require('@/assets/images/room12/4.jpg'),
+      require('@/assets/images/room12/5.jpg'),
+      require('@/assets/images/room12/6.jpg'),
+      require('@/assets/images/room12/7.jpg'),
+      require('@/assets/images/room12/8.jpg'),
+      require('@/assets/images/room12/9.jpg'),
+      require('@/assets/images/room12/10.jpg'),
+      require('@/assets/images/room12/11.jpg')
+    ],
+    additionContent: ['Просторный номер односпальными кроватями и уютными слип-боксами.']
   }
 ]
