a2f81f704a2abd72f3041ca0dc3b6efa10fc90d3
Nrect <boss.startov@bk.ru>
2021-04-22 13:43:11 +0200

    ~temp current-room

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -2,32 +2,32 @@
   .contacts
     .container
       .wrapper
-        .card-aboutUs(style="margin:0 auto;max-width:1000px")
-          .card-aboutUs__map
+        .card-about(style="margin:0 auto;max-width:1000px")
+          .card-about__map
             app-google-map-section(v-if="currentMap === 'google'")
             app-yandex-map-section(v-if="currentMap === 'yandex'")
-            .card-aboutUs__map-buttons
-              button.card-aboutUs__map-btn(@click="currentMap = 'google'")
+            .card-about__map-buttons
+              button.card-about__map-btn(@click="currentMap = 'google'")
                 img(src="@/assets/images/gmap.png")
-              button.card-aboutUs__map-btn(@click="currentMap = 'yandex'")
+              button.card-about__map-btn(@click="currentMap = 'yandex'")
                 img(src="@/assets/images/yamap.png")
-          .card-aboutUs__wrapper
-            .card-aboutUs__content
-              .card-aboutUs__contacts
-                .card-aboutUs__contact
-                  svg.card-aboutUs__contact-icon
+          .card-about__wrapper
+            .card-about__content
+              .card-about__contacts
+                .card-about__contact
+                  svg.card-about__contact-icon
                     use(xlink:href="#mobile-alt")
                   a(href="tel:+79637384646") +7963 738 46 46 (вайбер, вотсап)
-                .card-aboutUs__contact
-                  svg.card-aboutUs__contact-icon
+                .card-about__contact
+                  svg.card-about__contact-icon
                     use(xlink:href="#phone")
                   a(href="tel:+74012374646") +7 (4012) 37-46-46 (городской)
-                .card-aboutUs__contact
-                  svg.card-aboutUs__contact-icon
+                .card-about__contact
+                  svg.card-about__contact-icon
                     use(xlink:href="#envelop")
                   a(href="mailto:hostelpalmburg@gmail.com") hostelpalmburg@gmail.com
-                .card-aboutUs__contact
-                  svg.card-aboutUs__contact-icon
+                .card-about__contact
+                  svg.card-about__contact-icon
                     use(xlink:href="#map-marker")
                   | Адрес : п. Прибрежное, ул. Южное полукольцо, 4
 </template>
