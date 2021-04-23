50a5ae7155f231f71a7c3c1385de849d136d5359
Nrect <boss.startov@bk.ru>
2021-04-23 14:38:34 +0200

    gallery ~temp

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -3,6 +3,22 @@
     .container
       .wrapper
         app-about-us-card
+      .galery
+        .wrapper
+          h2.title Галерея
+          hooper.gallery__slider(:wheel-control="false" ref="carousel1" :settings="hooperSettings")
+            slide(v-for="(room,idx) in 10" :key="idx").gallery__slide
+              .gallery__img
+                img(:src="'http://lamaro.like-themes.com/wp-content/uploads/2018/02/service_02-430x280.jpg'")
+          .gallery__buttons
+            button(@click.prevent="slidePrev").gallery__btn
+              span
+                svg(style="transform: rotate(180deg);")
+                  use(xlink:href="#angle")
+            button(@click.prevent="slideNext").gallery__btn
+              span
+                svg
+                  use(xlink:href="#angle")
 
 </template>
 
@@ -11,6 +27,33 @@ import AppAboutUsCard from '~/components/about-us/AboutUsCard'
 
 export default {
   name: 'AboutUs',
-  components: { AppAboutUsCard }
+  components: { AppAboutUsCard },
+  data () {
+    return {
+      hooperSettings: {
+        pagination: 'no',
+        trimWhiteSpace: true,
+        breakpoints: {
+          1590: {
+            itemsToShow: 3
+          },
+          992: {
+            itemsToShow: 2
+          },
+          768: {
+            itemsToShow: 1
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    slidePrev () {
+      this.$refs.carousel1.slidePrev()
+    },
+    slideNext () {
+      this.$refs.carousel1.slideNext()
+    }
+  }
 }
 </script>
