42e987c90875281eba6e293a6321c583e6796761
Nrect <boss.startov@bk.ru>
2021-04-29 16:11:27 +0200

    Backgrounds

--- components/Intro.vue
+++ components/Intro.vue
@@ -23,9 +23,11 @@ export default {
     }
   },
   computed: {
+    getRouterParam () {
+      return this.$route.params.id
+    },
     getPageName () {
       const routeName = this.$route.name.slice(0, -5)
-      const routeParam = this.$route.params.id
       switch (routeName) {
         case 'index':
           return this.$t('introTitle.index')
@@ -40,7 +42,7 @@ export default {
         case 'contacts':
           return this.$t('introTitle.contacts')
         case 'rooms-id':
-          return this.getRoomName(routeParam)
+          return this.getRoomName(this.getRouterParam, 'title')
         default:
           return routeName
       }
@@ -54,29 +56,46 @@ export default {
         case 'rooms':
           return 'intro--rooms'
         case 'services':
-          return 'intro--services '
+          return 'intro--services'
         case 'faq':
           return 'intro--faq'
         case 'contacts':
           return 'intro--contacts '
+        case 'rooms-id':
+          return this.getRoomName(this.getRouterParam, 'image')
         default:
           return true
       }
     }
   },
   methods: {
-    getRoomName (param) {
-      switch (param) {
-        case 'two-room':
-          return this.$t('roomsTitles.titleTwo')
-        case 'six-room':
-          return this.$t('roomsTitles.titleSix')
-        case 'eight-room':
-          return this.$t('roomsTitles.titleEight')
-        case 'ten-room':
-          return this.$t('roomsTitles.titleTen')
-        case 'twelve-room':
-          return this.$t('roomsTitles.titleTwelve')
+    getRoomName (param, mode) {
+      if (mode === 'title') {
+        switch (param) {
+          case 'two-room':
+            return this.$t('roomsTitles.titleTwo')
+          case 'six-room':
+            return this.$t('roomsTitles.titleSix')
+          case 'eight-room':
+            return this.$t('roomsTitles.titleEight')
+          case 'ten-room':
+            return this.$t('roomsTitles.titleTen')
+          case 'twelve-room':
+            return this.$t('roomsTitles.titleTwelve')
+        }
+      } else if (mode === 'image') {
+        switch (param) {
+          case 'two-room':
+            return 'intro--two-room'
+          case 'six-room':
+            return 'intro--six-room'
+          case 'eight-room':
+            return 'intro--eight-room'
+          case 'ten-room':
+            return 'intro--ten-room'
+          case 'twelve-room':
+            return 'intro--twelve-room'
+        }
       }
     },
     ...mapActions('layout', ['toggleBookingModal'])
